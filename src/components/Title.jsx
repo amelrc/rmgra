bce9fc1f59189eed8d2dff43d94058ac86b29203
Amel Ramirez <amelrc@gmail.com>
2019-12-16 10:30:06 +0100

    adjusting indentation

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Title = ({title,subTitle}) => {
-	return ( 
+const Title = ({ title, subTitle }) => {
+	return (
 		<div className='title'>
 			<h2>{title}</h2>
 			<h3>{subTitle}</h3>
 		</div>
 	);
-}
- 
-export default Title
+};
+
+export default Title;
